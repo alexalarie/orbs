910cef6a2aad15c2f40453c154d01402e24ae39d
Alexandre Alarie <alexandre.alarie@gmail.com>
2023-12-16 12:00:46 -0500

    Fix PEP 440 error related to versioning

--- orbs/version.py
+++ orbs/version.py
@@ -11,7 +11,7 @@ else:
     repo_path = os.path.abspath(os.path.dirname(__file__) + os.sep + '..')
     try:
         repo = git.Repo(repo_path)
-        __version__ = repo.active_branch.name + '.' + repo.git.rev_parse(repo.head.object.hexsha, short=4)
+        __version__ =  "0.0.0" + '+' + repo.git.rev_parse(repo.head.object.hexsha, short=4)
     except git.GitError:
         pass
 
